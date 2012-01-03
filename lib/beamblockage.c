06ae0bcf3d5cefb4596a4873dcb397c4297a2d28
Anders Henja <anders@baltrad.eu>
2012-01-03 08:54:32 +0100

    Ticket 733: Memory leak in beamblockage.c @381

--- lib/beamblockage.c
+++ lib/beamblockage.c
@@ -383,6 +383,7 @@ int BeamBlockage_setCacheDirectory(BeamBlockage_t* self, const char* cachedir)
       goto done;
     }
   }
+  RAVE_FREE(self->cachedir);
   self->cachedir = tmp;
   tmp = NULL; // Release responsibility for memory
   result = 1;
